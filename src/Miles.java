9569338c786f07f5883603cc00245dce9ee1be78
Andrey Shitikov <andrey.shitikoff@gmail.com>
2020-09-18 10:25:39 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,8 @@
+public class Miles {
+    public static void main(String[] args) {
+        long amount = 2099999;
+
+        long miles = amount / 20 / 100;
+        System.out.println(miles);
+    }
+}
